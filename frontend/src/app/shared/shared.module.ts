4803e96ba8575fd54fdb794b996b7a1deda0bcdb
Alexander Braginsky <bro@beam-mw.com>
2019-03-28 12:13:24 +0300

    wallet manager ui

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StatusTitleComponent } from './components';
-import { DataTableComponent } from './components/data-table/data-table.component';
 
 @NgModule({
   declarations: [
